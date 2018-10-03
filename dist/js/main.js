f644a4f5516a8c07576aa1e108dff65f4346a162
tshatmanaliev <tshatmanaliev@gmail.com>
2018-10-03 18:28:51 +0600

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,8 @@
 // Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.btn-nav');
-const menuBranding = document.querySelector('.btn-branding');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 //SEt Initial State Of Menu
